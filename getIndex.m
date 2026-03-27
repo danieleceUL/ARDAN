b13d1140949d8f5260f58c8be31b17f2d3b97135
Daniel Jakab <113525778+danieleceUL@users.noreply.github.com>
2026-03-27 13:53:50 +0000

    Add metadata comments to getIndex.m
    Added header comments with metadata for the getIndex function.

--- getIndex.m
+++ getIndex.m
@@ -1,3 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Date: 2024
+%   Name: Daniel Jakab
+%   Description: Assigns an index to all extracted data points in NS-SFR
+%                analysis.
+%   Copyright (c) 2023 D. Jakab
+%   UNIVERSITY OF Limerick PhD Research
+%              - D2ICE Research Group
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function idx = getIndex(nx,ny,cols,rows,xpt,ypt)
     xedge = linspace(0,cols,nx+1);
     yedge = linspace(0,rows,ny+1);
